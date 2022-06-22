3a1ca50099601f890bcde814c10e8932fbb575b0
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-06-22 11:11:23 +0300

    ✨ feat: add favorite food

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log("Microservices rock!", favoriteFood);
     await sleep(5000);
   }
 }
